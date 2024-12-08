8ce9189842b9d305294ae6619cad3e4300a75e94
FredymarLeon <fredymar@gmail.com>
2024-12-07 21:28:14 -0300

    Implementação de navegação desde Home para Investimentos.

--- app/src/main/java/com/fleon/clonedoaplicativodobancopan/fragments/HomeFragment.kt
+++ app/src/main/java/com/fleon/clonedoaplicativodobancopan/fragments/HomeFragment.kt
@@ -18,7 +18,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         homeBinding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
