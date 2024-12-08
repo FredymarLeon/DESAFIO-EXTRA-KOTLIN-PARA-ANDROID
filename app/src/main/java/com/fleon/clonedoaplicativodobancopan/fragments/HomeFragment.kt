34f052d1eb3adac0044d98030c5411a22f4272a1
FredymarLeon <fredymar@gmail.com>
2024-12-07 21:13:46 -0300

    Implementação de navegação desde Home para Cartão.

--- app/src/main/java/com/fleon/clonedoaplicativodobancopan/fragments/HomeFragment.kt
+++ app/src/main/java/com/fleon/clonedoaplicativodobancopan/fragments/HomeFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.fleon.clonedoaplicativodobancopan.R
+import com.fleon.clonedoaplicativodobancopan.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
+    private var homeBinding: FragmentHomeBinding? = null
+    private val binding get() = homeBinding!!
 
 
     override fun onCreateView(
@@ -16,6 +20,14 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        homeBinding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.cvCard.setOnClickListener{
+            it.findNavController().navigate(R.id.action_homeFragment_to_cardFragment)
+        }
+
     }
 }
